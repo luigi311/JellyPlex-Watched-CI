6ee48f986cb03275fa37dd4929f865048f6691a2
Luigi311 <git@luigi311.com>
2023-12-06 12:52:59 -0700

    Add some watched media

--- jellyfin/config/data/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
+++ jellyfin/config/data/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2023-12-06T19:26:06.2279628Z","EndTimeUtc":"2023-12-06T19:26:06.8721085Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
+{"StartTimeUtc":"2023-12-06T19:36:32.1046318Z","EndTimeUtc":"2023-12-06T19:36:32.7604487Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}

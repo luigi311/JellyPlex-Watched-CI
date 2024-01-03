11d008b7533aabbc87cbdd3a9fb7beca1a68a581
Luigi311 <git@luigi311.com>
2024-01-02 20:56:39 -0700

    Update
    Signed-off-by: Luigi311 <git@luigi311.com>

--- jellyfin/config/data/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
+++ jellyfin/config/data/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2024-01-03T03:22:10.1704981Z","EndTimeUtc":"2024-01-03T03:22:10.883558Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
+{"StartTimeUtc":"2024-01-03T03:33:17.0916843Z","EndTimeUtc":"2024-01-03T03:33:17.7415589Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}

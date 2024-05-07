174ac5584d60402b34e4584a184f561367bb2be7
Luis Garcia <git@luigi311.com>
2024-05-06 21:39:39 -0600

    Update scripts

--- start_containers.sh
+++ start_containers.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-sudo chown -R "${PUID}:${PGID}" ./*
+# Get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export PGID
+PGID=$(id -g)
+export PUID
+PUID=$(id -u)
+
+sudo chown -R "${PUID}:${PGID}" "${DIR}"/*
 
 docker pull lscr.io/linuxserver/plex &
 docker pull lscr.io/linuxserver/jellyfin &
@@ -8,6 +16,6 @@ docker pull emby/embyserver &
 
 wait
 
-docker-compose -f plex/docker-compose.yml up -d 
-docker-compose -f jellyfin/docker-compose.yml up -d
-docker-compose -f emby/docker-compose.yml up -d
+docker-compose -f "${DIR}/plex/docker-compose.yml" up -d 
+docker-compose -f "${DIR}/jellyfin/docker-compose.yml" up -d
+docker-compose -f "${DIR}/emby/docker-compose.yml" up -d
--- update_containers.sh
+++ update_containers.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+# Get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 export PGID
 PGID=$(id -g)
 export PUID
 PUID=$(id -u)
 
-./start_containers.sh
+# Run DIR/start_containers.sh
+"${DIR}/start_containers.sh"
 
 # Wait for containers to start
 sleep 15
 
-docker-compose -f plex/docker-compose.yml down
-docker-compose -f jellyfin/docker-compose.yml down
-docker-compose -f emby/docker-compose.yml down
+docker-compose -f "${DIR}/plex/docker-compose.yml" down
+docker-compose -f "${DIR}/jellyfin/docker-compose.yml" down
+docker-compose -f "${DIR}/emby/docker-compose.yml" down

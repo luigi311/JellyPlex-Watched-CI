0ef463b878ab51a71481a3061d39e2ae283cc56b
Luis Garcia <git@luigi311.com>
2024-06-02 18:39:00 -0600

    Add stop_containers.sh
    Signed-off-by: Luis Garcia <git@luigi311.com>

---
+++ stop_containers.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export PGID
+PGID=$(id -g)
+export PUID
+PUID=$(id -u)
+
+docker-compose -f "${DIR}/plex/docker-compose.yml" down
+docker-compose -f "${DIR}/jellyfin/docker-compose.yml" down
+docker-compose -f "${DIR}/emby/docker-compose.yml" down
